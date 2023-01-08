0e88ee3145c6f867c102995a7630ab1ab9cecf77
Aaron Porter <aaron.w.porter@gmail.com>
2023-01-08 12:26:01 -0800

    Add initial header component

---
+++ components/Header.tsx
@@ -0,0 +1,16 @@
+import { ClipboardDocumentCheckIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <div className="flex h-24 flex-wrap content-center justify-between bg-slate-100">
+      <Link href="/" className="flex">
+        <p className="ml-6 text-2xl">TaskSimple</p>
+        <ClipboardDocumentCheckIcon className="mt-1.5 h-6 w-6" />
+      </Link>
+      <Link href="#" className="mr-6" type="button">
+        My Account
+      </Link>
+    </div>
+  );
+}
